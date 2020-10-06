13ccc0614dc5dfdec8af437d899a871f68d75a24
William Teles <teles967@gmail.com>
2020-10-06 15:19:22 -0300

    Adicionando o JMenuBar.

--- src/atividade6/Cadastro.java
+++ src/atividade6/Cadastro.java
@@ -84,7 +84,7 @@ public class Cadastro extends JPanel implements ActionListener {
         buttonSalvar.setToolTipText("Clique neste butão para salvar seus dados.");
         buttonFechar.setToolTipText("Clique neste butão para fechar essa tela de cadastro.");
         
-        //Add Components to this container, using the default GridLayout.
+        //Add Components to this container, using the GridLayout.
         GridLayout layout = new GridLayout(9,0);
         setLayout(layout);
         layout.setVgap(30);
@@ -156,6 +156,8 @@ public class Cadastro extends JPanel implements ActionListener {
         //Create and set up the content pane.
         Cadastro newContentPane = new Cadastro();
         newContentPane.setOpaque(true); //content panes must be opaque
+        MenuLookDemo demo = new MenuLookDemo();
+        frame.setJMenuBar(demo.createMenuBar());
         frame.setContentPane(newContentPane);
 
         //Display the window.
@@ -172,5 +174,93 @@ public class Cadastro extends JPanel implements ActionListener {
                 createAndShowGUI();
             }
         });
+    }
+}
+class MenuLookDemo {
+    JTextArea output;
+    JScrollPane scrollPane;
+
+    public JMenuBar createMenuBar() {
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem;
+        JRadioButtonMenuItem rbMenuItem;
+        JCheckBoxMenuItem cbMenuItem;
+
+        //Create the menu bar.
+        menuBar = new JMenuBar();
+
+        //Build the first menu.
+        menu = new JMenu("A Menu");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        menuBar.add(menu);
+
+        //a group of JMenuItems
+        menuItem = new JMenuItem("A text-only menu item",
+                KeyEvent.VK_T);
+        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_1, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription(
+                "This doesn't really do anything");
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem("Both text and icon");
+        menuItem.setMnemonic(KeyEvent.VK_B);
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem();
+        menuItem.setMnemonic(KeyEvent.VK_D);
+        menu.add(menuItem);
+
+        //a group of radio button menu items
+        menu.addSeparator();
+        ButtonGroup group = new ButtonGroup();
+
+        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
+        rbMenuItem.setSelected(true);
+        rbMenuItem.setMnemonic(KeyEvent.VK_R);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        rbMenuItem = new JRadioButtonMenuItem("Another one");
+        rbMenuItem.setMnemonic(KeyEvent.VK_O);
+        group.add(rbMenuItem);
+        menu.add(rbMenuItem);
+
+        //a group of check box menu items
+        menu.addSeparator();
+        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
+        cbMenuItem.setMnemonic(KeyEvent.VK_C);
+        menu.add(cbMenuItem);
+
+        cbMenuItem = new JCheckBoxMenuItem("Another one");
+        cbMenuItem.setMnemonic(KeyEvent.VK_H);
+        menu.add(cbMenuItem);
+
+        //a submenu
+        menu.addSeparator();
+        submenu = new JMenu("A submenu");
+        submenu.setMnemonic(KeyEvent.VK_S);
+
+        menuItem = new JMenuItem("An item in the submenu");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Another item");
+        submenu.add(menuItem);
+        menu.add(submenu);
+
+        //Build second menu in the menu bar.
+        menu = new JMenu("Another Menu");
+        menu.setMnemonic(KeyEvent.VK_N);
+        menu.getAccessibleContext().setAccessibleDescription(
+                "This menu does nothing");
+        menuBar.add(menu);
+
+        return menuBar;
     }
 }
