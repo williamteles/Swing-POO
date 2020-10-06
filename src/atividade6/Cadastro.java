785a3b7ed2fed7800b2044699babaf74657a70cd
gabriel lima <gabriellima.glgs@gmail.com>
2020-10-06 15:55:53 -0300

    menu e submenus descritos e nomes da variaveis mais legivel

--- src/atividade6/Cadastro.java
+++ src/atividade6/Cadastro.java
@@ -156,8 +156,8 @@ public class Cadastro extends JPanel implements ActionListener {
         //Create and set up the content pane.
         Cadastro newContentPane = new Cadastro();
         newContentPane.setOpaque(true); //content panes must be opaque
-        MenuLookDemo demo = new MenuLookDemo();
-        frame.setJMenuBar(demo.createMenuBar());
+        Menu menu = new Menu();
+        frame.setJMenuBar(menu.createMenuBar());
         frame.setContentPane(newContentPane);
 
         //Display the window.
@@ -176,7 +176,7 @@ public class Cadastro extends JPanel implements ActionListener {
         });
     }
 }
-class MenuLookDemo {
+class Menu {
     JTextArea output;
     JScrollPane scrollPane;
 
@@ -184,82 +184,39 @@ class MenuLookDemo {
         JMenuBar menuBar;
         JMenu menu, submenu;
         JMenuItem menuItem;
-        JRadioButtonMenuItem rbMenuItem;
-        JCheckBoxMenuItem cbMenuItem;
-
+        
         //Create the menu bar.
         menuBar = new JMenuBar();
 
         //Build the first menu.
-        menu = new JMenu("A Menu");
-        menu.setMnemonic(KeyEvent.VK_A);
+        menu = new JMenu("Menu");
         menu.getAccessibleContext().setAccessibleDescription(
                 "The only menu in this program that has menu items");
         menuBar.add(menu);
 
-        //a group of JMenuItems
-        menuItem = new JMenuItem("A text-only menu item",
-                KeyEvent.VK_T);
-        //menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_1, ActionEvent.ALT_MASK));
-        menuItem.getAccessibleContext().setAccessibleDescription(
-                "This doesn't really do anything");
-        menu.add(menuItem);
+        submenu = new JMenu("File");
 
-        menuItem = new JMenuItem("Both text and icon");
-        menuItem.setMnemonic(KeyEvent.VK_B);
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem();
-        menuItem.setMnemonic(KeyEvent.VK_D);
-        menu.add(menuItem);
-
-        //a group of radio button menu items
-        menu.addSeparator();
-        ButtonGroup group = new ButtonGroup();
-
-        rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_R);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        rbMenuItem = new JRadioButtonMenuItem("Another one");
-        rbMenuItem.setMnemonic(KeyEvent.VK_O);
-        group.add(rbMenuItem);
-        menu.add(rbMenuItem);
-
-        //a group of check box menu items
-        menu.addSeparator();
-        cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
-        cbMenuItem.setMnemonic(KeyEvent.VK_C);
-        menu.add(cbMenuItem);
-
-        cbMenuItem = new JCheckBoxMenuItem("Another one");
-        cbMenuItem.setMnemonic(KeyEvent.VK_H);
-        menu.add(cbMenuItem);
-
-        //a submenu
-        menu.addSeparator();
-        submenu = new JMenu("A submenu");
-        submenu.setMnemonic(KeyEvent.VK_S);
-
-        menuItem = new JMenuItem("An item in the submenu");
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_2, ActionEvent.ALT_MASK));
+        menuItem = new JMenuItem("Import");
         submenu.add(menuItem);
 
-        menuItem = new JMenuItem("Another item");
+        menuItem = new JMenuItem("Export");
         submenu.add(menuItem);
         menu.add(submenu);
 
         //Build second menu in the menu bar.
-        menu = new JMenu("Another Menu");
-        menu.setMnemonic(KeyEvent.VK_N);
+        menu = new JMenu("Tools");
         menu.getAccessibleContext().setAccessibleDescription(
                 "This menu does nothing");
         menuBar.add(menu);
+
+        submenu = new JMenu("Edit");
+
+        menuItem = new JMenuItem("Copy");
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Paste");
+        submenu.add(menuItem);
+        menu.add(submenu);
 
         return menuBar;
     }
